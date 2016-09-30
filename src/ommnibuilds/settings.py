b78c20e56c41e9f23693b3bc473cdd4d09f10c86
jwagstaff <jeremiah.wagstaff@gmail.com>
2016-09-30 12:20:31 -0500

    setup database

--- src/ommnibuilds/settings.py
+++ src/ommnibuilds/settings.py
@@ -75,11 +75,14 @@ WSGI_APPLICATION = 'ommnibuilds.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'staging',
+        'USER':  os.environ['STAGE_RDS_USER'],
+        'PASSWORD':  os.environ['STAGE_RDS_PASS'],
+        'HOST': 'ob-rds-staging.crcuzaciir8d.us-west-2.rds.amazonaws.com',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
